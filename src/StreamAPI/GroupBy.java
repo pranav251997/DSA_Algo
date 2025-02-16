991561b4a9728d0893488a3c4de020a1629db095
Pranav Vispute <pranavvispute1@gmail.com>
2025-02-17 00:22:27 +0530

    Add GroupBy API

--- src/StreamAPI/GroupBy.java
+++ src/StreamAPI/GroupBy.java
@@ -1,7 +1,10 @@
 package StreamAPI;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GroupBy {
 
@@ -15,6 +18,37 @@ public class GroupBy {
 		Employee e6 = new Employee(6, "Frank", "Finance", 82000);
 		List<Employee> employees = Arrays.asList(e1, e2, e3, e4, e5, e6);
 
+		// How do you group a list of objects by an attribute in Java using streams?
+		Map<String, List<Employee>> groupByDep = employees.stream()
+				.collect(Collectors.groupingBy(Employee::getDepartment));
+		System.out.println(groupByDep);
+
+		// Count the number of employees in each department.
+		Map<String, Long> countPerDep = employees.stream()
+				.collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+		System.out.println(countPerDep);
+
+		// Find the total salary expense per department.
+		Map<String, Double> totalSalPerDep = employees.stream()
+				.collect(Collectors.groupingBy(Employee::getDepartment, Collectors.summingDouble(Employee::getSalary)));
+		System.out.println(totalSalPerDep);
+
+		// Group employees name by department.
+		Map<String, List<String>> groupByEmpNameDep = employees.stream().collect(Collectors
+				.groupingBy(Employee::getDepartment, Collectors.mapping(Employee::getName, Collectors.toList())));
+		System.out.println(groupByEmpNameDep);
+
+		// Find the average salary per department.
+		Map<String, Double> avgPayPerDep = employees.stream().collect(
+				Collectors.groupingBy(Employee::getDepartment, Collectors.averagingDouble(Employee::getSalary)));
+		System.out.println(avgPayPerDep);
+
+		// Get the highest-paid employee per department.
+		Map<String, String> highestPaidEmpByDep = employees.stream()
+				.collect(Collectors.groupingBy(Employee::getDepartment,
+						Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary)),
+								emp -> emp.map(Employee::getName).orElse(null))));
+		System.out.println(highestPaidEmpByDep);
 	}
 
 }
